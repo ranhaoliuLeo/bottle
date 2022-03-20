1b7dc3c3e3ceaaa48bd0549eb794d5f3b70fb2c3
ranhaoliu <ranhaoliu@tencent.com>
2022-03-21 00:56:30 +0800

    feat: temp commit
    that will combine with other commits.

--- bottle.go
+++ bottle.go
@@ -1,7 +1,9 @@
 package bottle
 
 import (
+	"log"
 	"os"
+	"syscall"
 	"unsafe"
 
 	"github.com/ranhaoliuLeo/bottle/bpage"
@@ -17,7 +19,7 @@ type DB struct {
 }
 
 func Open(path string, mode os.FileMode) (*DB, error) {
-	file, err := os.OpenFile(path, os.O_CREATE, mode)
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE, mode)
 	if err != nil {
 		return nil, err
 	}
@@ -30,18 +32,37 @@ func Open(path string, mode os.FileMode) (*DB, error) {
 	}
 	if fileDescInfo.Size() == 0 {
 		// means that file is a brand new file
-		dbIns.init()
-
+		if err := dbIns.init(); err != nil {
+			log.Fatalf("db init fail, err: %v", err)
+			return nil, err
+		}
 	} else {
 		// means that file is a old file,
+		// so we should read the first files meta page.
+		buf := make([]byte, 0x1000)
+		if _, err := dbIns.file.ReadAt(buf[:], 0); err != nil {
+			meta := dbIns.getPageFromBuffer(buf, 0).GetMeta()
+			if err := meta.Check(); err != nil {
+				log.Printf("Err Check db file, err: %v, maybe page size was wrong. plz try agin", err)
+				dbIns.pageSize = os.Getpagesize()
+			}
+			dbIns.pageSize = int(meta.PageSize)
+		}
+
 	}
+
 	return dbIns, nil
 }
 
 // dbInit if file was the created, you should init the file.
-func (db *DB) init() {
+func (db *DB) init() error {
 	db.pageSize = os.Getpagesize()
 	buf := make([]byte, db.pageSize*4)
+	// the first page will contain the db meta info
+	// the second page will contain the same data. that will
+	// 0 is meta 1 is meta. so the freelist page will start at num 2.This is the cs's consensus.
+	// so the 2 is the freelist page. and the third page will save the db data's root.And root is the begin of all data searching.
+	// and data page will begin with 4.
 	for i := 0; i < 2; i++ {
 		pid := bpage.ID(i)
 		page := db.getPageFromBuffer(buf, pid)
@@ -57,11 +78,24 @@ func (db *DB) init() {
 		meta.TxID = bpage.TxID(i)
 		meta.Checksum = meta.GenSum64()
 	}
+	page := db.getPageFromBuffer(buf, bpage.ID(2))
+	page.ID = bpage.ID(2)
+	page.Flags = bpage.FreelistFlag
+	page.Count = 0
 
+	if _, err := db.file.WriteAt(buf, 0); err != nil {
+		return err
+	}
+	return fdatasync(db)
 }
 
 // getPageFromBuffer: get page from buffer, trans the byte to struct
 func (db *DB) getPageFromBuffer(buf []byte, pgID bpage.ID) *bpage.Data {
 	pageBytes := &buf[pgID*bpage.ID(pgID)]
 	return (*bpage.Data)(unsafe.Pointer(pageBytes))
+}
+
+// fdatasync: this system call will just sync the data to the file.And not sync the meta of the file.
+func fdatasync(db *DB) error {
+	return syscall.Fdatasync(int(db.file.Fd()))
 }
--- bpage/bpage.go
+++ bpage/bpage.go
@@ -3,6 +3,7 @@ package bpage
 import (
 	"hash/fnv"
 	"unsafe"
+	"errors"
 )
 
 type ID uint64
@@ -15,6 +16,8 @@ const (
 	MetaFlag     = 0x04
 	FreelistFlag = 0x10
 )
+
+const magic uint32 = 0xED0CDBBD
 
 // Data a page of data; It may presentation some data in the disk.
 type Data struct {
@@ -52,4 +55,21 @@ func (m *Meta) GenSum64() uint64 {
 	var h = fnv.New64a()
 	_, _ = h.Write((*[unsafe.Offsetof(Meta{}.Checksum)]byte)(unsafe.Pointer(m))[:])
 	return h.Sum64()
+}
+
+var (
+	ErrInvalid = errors.New("invalid database")
+	ErrVersionMismatch = errors.New("version mismatch")
+	ErrChecksum = errors.New("checksum error")
+)
+
+func (m *Meta) Check() error {
+	if m.Magic != magic {
+		return ErrInvalid
+	} else if m.Version != 2 {
+		return ErrVersionMismatch
+	} else if m.Checksum != 0 && m.Checksum != m.GenSum64() {
+		return ErrChecksum
+	}
+	return nil
 }
